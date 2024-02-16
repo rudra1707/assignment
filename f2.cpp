218a9edb08a2ffe9d13189df6a16e588886e57d2
Rudra Bhatt <bhattrudra17072005@gmail.com>
2024-02-15 22:57:48 -0500

    added all required comments

--- f2.cpp
+++ f2.cpp
@@ -1,9 +1,10 @@
 /*
 * FILE: f2.cpp
-*
+* PROJECT: FOCUSED ASSIGNMENT 2
 * PROGRAMMER: RUDRA NITESHKUMAR BHATT
 * FIRST VERSION : 2024-01-24
-* 
+* student id:8980507
+* DESCRIPTION: Use Loops and Arithmetic operators to produce and display a sum of numbers from 1.
 */
 
 
