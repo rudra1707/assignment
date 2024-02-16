5778d0cfdeaf56d0d7accedf177323b5162cdbc3
Rudra Bhatt <bhattrudra17072005@gmail.com>
2024-02-15 22:54:51 -0500

    solved logical error

--- f2.cpp
+++ f2.cpp
@@ -1,10 +1,16 @@
-
+/*
+* FILE: f2.cpp
+*
+* PROGRAMMER: RUDRA NITESHKUMAR BHATT
+* FIRST VERSION : 2024-01-24
+* 
+*/
 
 
 #include<stdio.h>
 int main()
 {
-	int N = 1, S = 1;
+	int N = 1, S = 0;
 	while (N <= 500)
 	{
 		S = S + N;
